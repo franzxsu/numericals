e9709169b42798dfe54a7becfdca00bb29efc44a
franzxsu <2225292@slu.edu.ph>
2023-11-11 19:08:32 +0800

    api is now working

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,11 +1,14 @@
 # THIS WEHERE THE MAIN APPLICATION
 
-from flask import Flask
-from .receiver.methods import numerical_methods_blueprint
+from flask import Flask, request
+from flask_cors import CORS
+from backend.api.receiver.methods import numerical_methods_blueprint
 
 app = Flask(__name__)
+CORS(app)
 
 app.register_blueprint(numerical_methods_blueprint)
+#app.logger.info(f"Received request: {request.json}")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5000)

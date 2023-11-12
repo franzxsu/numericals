8e9d9af3871046081edac4b83146ad90236f6c51
franzxsu <2225292@slu.edu.ph>
2023-11-12 20:44:21 +0800

    scalar_multiplication method matrix is 0 based, row interchange is not lol i cant decide what to use

--- backend/api/services/midterms/matrix.py
+++ backend/api/services/midterms/matrix.py
@@ -1,3 +1,6 @@
+import re
+
+
 class Matrix:
     def __init__(self, rows, cols):
         self.rows = rows
@@ -27,13 +30,31 @@ class Matrix:
     def __str__(self):
         return "\n".join([" ".join(map(str, row)) for row in self.data])
 
-# ELEMENTARY ROW OPERATIONS
+    # ELEMENTARY ROW OPERATIONS
 
     def row_interchange(self, row1, row2):
         if 0 <= row1 < self.rows and 0 <= row2 < self.rows:
             self.data[row1], self.data[row2] = self.data[row2], self.data[row1]
         else:
             raise ValueError("ROW OUT OF RANGE")
+
+    # function can be for example, 2r1 which means 2 x r1 and then the second parameter is where the answer of the
+    # scalar multiplication will go, if this is blank, answers will go to the row where the multiplication is called
+    # not sure if this is correct doe
+    def scalar_multiplication(self, function, row_where_u_put_le_answer=None):
+        match = re.match(r'(\d+)([rR])(\d+)', function)
+        if not match:
+            raise ValueError("Invalid scalar multiplication function format")
+
+        scalar, _, row_index = match.groups()
+        scalar = float(scalar)
+        row_index = int(row_index) - 1
+
+        if 0 <= row_index < self.rows:
+            target_row = row_index if row_where_u_put_le_answer is None else row_where_u_put_le_answer
+            self.data[target_row] = [scalar * element for element in self.data[target_row]]
+        else:
+            raise ValueError("Invalid row index")
 
 # for i in range(rows):
 #     for j in range(cols):
--- backend/api/services/tester.py
+++ backend/api/services/tester.py
@@ -19,10 +19,19 @@ a = linear_interpolation([0, 1, 2], [0, 3, 2], 1.5)
 for q in a:
     print(a)
 
-# matrix = Matrix(3, 3)
-# print("BEFORE INTERCHANGE: ")
-# print(matrix)
-#
-# print("AFTER INTERCHANGE: ")
-# matrix.row_interchange(2,1)
-# print(matrix)
+# ROWS AND COLS ARE 0 BASED PARANG ARRAY
+matrix = Matrix(2, 2)
+print("BEFORE INTERCHANGE: ")
+print(matrix)
+
+print("AFTER INTERCHANGE: ")
+matrix.row_interchange(0,1)
+print(matrix)
+
+print("THIS IS MATRIX NOW:")
+print(matrix)
+matrix.scalar_multiplication("2r1", 1)
+print("B")
+print(matrix)
+matrix.scalar_multiplication("1r2", 0)
+print(matrix)

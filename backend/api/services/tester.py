ca8a33b365303cf40192962787c95d779671520d
franzxsu <2225292@slu.edu.ph>
2023-11-13 05:40:44 +0800

    fixed json return, javascript kinda buggin on parentheses on jsons

--- backend/api/services/tester.py
+++ backend/api/services/tester.py
@@ -20,18 +20,20 @@ for q in a:
     print(a)
 
 # ROWS AND COLS ARE 0 BASED PARANG ARRAY
-matrix = Matrix(2, 2)
-print("BEFORE INTERCHANGE: ")
-print(matrix)
 
-print("AFTER INTERCHANGE: ")
-matrix.row_interchange(0,1)
-print(matrix)
 
-print("THIS IS MATRIX NOW:")
-print(matrix)
-matrix.scalar_multiplication("2r1", 1)
-print("B")
-print(matrix)
-matrix.scalar_multiplication("1r2", 0)
-print(matrix)
+# matrix = Matrix(2, 2)
+# print("BEFORE INTERCHANGE: ")
+# print(matrix)
+#
+# print("AFTER INTERCHANGE: ")
+# matrix.row_interchange(0,1)
+# print(matrix)
+#
+# print("THIS IS MATRIX NOW:")
+# print(matrix)
+# matrix.scalar_multiplication("2r1", 1)
+# print("B")
+# print(matrix)
+# matrix.scalar_multiplication("1r2", 0)
+# print(matrix)

06f80160e82309eecd97c653832a298cc54cb2f1
franzxsu <2225292@slu.edu.ph>
2023-11-14 00:24:50 +0800

    minor cleanup

--- backend/api/receiver/methods.py
+++ backend/api/receiver/methods.py
@@ -7,17 +7,6 @@ from backend.api.services.midterms.matrix import *
 numerical_methods_blueprint = Blueprint('numerical_methods', __name__)
 
 
-@numerical_methods_blueprint.route('/compute', methods=['POST'])
-def compute():
-    data = request.json
-    print(data)
-    numbers = data.get('numbers')
-    method = data.get('method')
-
-    result = perform_calculation(numbers, method)
-    return jsonify({"result": result})
-
-
 @numerical_methods_blueprint.route('/bisection', methods=['POST'])
 def bisection():
     data = request.json
@@ -31,7 +20,6 @@ def bisection():
 
     result = bisection_method(str(function), int(lower_bound), int(upper_bound))
 
-    # result = perform_calculation(numbers, method)
     return jsonify({"result": result})
 
 
@@ -47,6 +35,7 @@ def newtonraphson():
     result = newton_raphson_method(str(function), int(x))
     return jsonify({"result": result})
 
+
 @numerical_methods_blueprint.route('/matrix_operation', methods=['POST'])
 def matrix_operation():
     data = request.json
@@ -54,15 +43,8 @@ def matrix_operation():
     matrix2 = data.get('matrix2')
     operation = data.get('operation')
 
-    print(matrix1)
-
     matrix1_obj = Matrix.from_json(str(matrix1))
     matrix2_obj = Matrix.from_json(str(matrix2))
 
     result = matrix1_obj.matrix_operation(matrix2_obj, operation)
     return jsonify({"result": result})
-
-    # matrix2_obj = Matrix.from_json(matrix2)
-    # print(matrix1_obj)
-    # result = newton_raphson_method()
-    # return jsonify({"result": result})

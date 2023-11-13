8bbe7869e5a70460d5ce5a7acd9c891e4cd939bb
franzxsu <2225292@slu.edu.ph>
2023-11-13 17:40:11 +0800

    size of matrix now dynamically changes depending on input

---
+++ frontend/js/matrices.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const maxMatrixSize = 7;
+    const rowInput = document.getElementById('matrixrow');
+    const columnInput = document.getElementById('matrixcolumn');
+
+    // WILL CHANGE SIZE OF MATRIX DEPENDING ON ZEEEE INPUT ^^^^
+    function updateMatrixSize() {
+        const rows = Math.min(parseInt(rowInput.value) || 0, maxMatrixSize);
+        const columns = Math.min(parseInt(columnInput.value) || 0, maxMatrixSize);
+
+        updateMatrix('matrix1', rows, columns);
+        updateMatrix('matrix2', rows, columns);
+    }
+
+    function updateMatrix(matrixId, rows, columns) {
+        const matrix = document.getElementById(matrixId);
+        matrix.innerHTML = '';
+
+        for (let r = 1; r <= rows; r++) {
+            const matrixRow = document.createElement('div');
+            matrixRow.className = 'matrix-row';
+            matrixRow.id = `${matrixId}row${r}`;
+
+            for (let c = 1; c <= columns; c++) {
+                const input = document.createElement('input');
+                input.type = 'text';
+                input.id = `${matrixId.slice(0, 2)}r${r}c${c}`;
+                input.placeholder = 'column';
+                matrixRow.appendChild(input);
+            }
+
+            matrix.appendChild(matrixRow);
+        }
+    }
+
+    rowInput.addEventListener('input', updateMatrixSize);
+    columnInput.addEventListener('input', updateMatrixSize);
+});
